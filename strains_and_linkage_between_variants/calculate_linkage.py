558a11bcf75683743202e8b303a125d5e4be8e6f
noamharel161 <36629785+noamharel161@users.noreply.github.com>
2019-08-05 16:26:34 +0300

    Update calculate_linkage.py

--- strains_and_linkage_between_variants/calculate_linkage.py
+++ strains_and_linkage_between_variants/calculate_linkage.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
+#! /usr/local/python_anaconda/bin/python3.4
 
-import sys
 import pandas as pd
-import itertools
-import os
 import numpy as np
 import argparse
 
@@ -24,55 +21,35 @@ def mutation_pairings_statistics(args):
     A535.0G
     T1764.0-"
     '''
-    blast_df = pd.read_csv(args.input_blast_df)
-    mutations_df = pd.read_csv(args.input_mutation_df)
     recognized_mutations = pd.read_csv(args.input_chosen_mutations)['variant'].tolist()
-
-    
+    recognized_positions = [float(p[1:-1]) for p in recognized_mutations]
+    blast_df = pd.read_csv(args.input_blast_df)
     # choose only reads that were mapped only once in blast
     blast_df['read_count'] = blast_df.groupby('read')['start_ref'].transform('count')
     blast_df = blast_df[(blast_df.read_count == 1)]
-  
+    
+    # choose only reads that are mapped from at least start_pos_read to end_pos_read
+    blast_df = blast_df[(blast_df.start_ref < min(recognized_positions)) & (blast_df.end_ref > max(recognized_positions))]
+    blast_df = blast_df[['read', 'start_ref', 'end_ref']]
+    
+    mutations_df = pd.read_csv(args.input_mutation_df)
+    mutations_df = mutations_df[(mutations_df.ref != '-')] # remove insertions
+    
+    # drop reads containing a variation that is not the recognized mutation or the WT in the positions we are checking combinations for.
+    reads_to_drop = mutations_df[(mutations_df.position.isin(recognized_positions)) & ~(mutations_df.full_mutation.isin(recognized_mutations))].read.tolist()
+    blast_df = blast_df[~(blast_df.read.isin(reads_to_drop))]
+    
+    mutations_df = mutations_df[mutations_df.full_mutation.isin(recognized_mutations)]
+    relevant_mutations = pd.merge(mutations_df, blast_df[['read']], how='right', on='read').fillna(0)
+    print(len(relevant_mutations.read.drop_duplicates()))
     # conditional statistics
     matrix = pd.DataFrame(np.zeros(shape=(len(recognized_mutations)*2 + 1,len(recognized_mutations)*2)), columns = recognized_mutations + [f[1:-1].split('.')[0] for f in recognized_mutations], index = recognized_mutations + [f[1:-1].split('.')[0] for f in recognized_mutations] + ['P(column)'])
     for mutation_1 in recognized_mutations:
-        b = blast_df.copy()
-        m = mutations_df.copy()
-        # keep only alignments containing both positions for mutations 1 and 2.
-        b = b[((b.start_ref < int(mutation_1[1:-1].split('.')[0])) & (b.end_ref > int(mutation_1[1:-1].split('.')[0])))]
-        b = b[['read']]
-        # look only at mutations in positions i and j
-        m = m[m.position.isin([int(mutation_1[1:-1].split('.')[0])])]
-        # keep all reads that contain positions i and j (both containing mutations in i and/or j 
-        # and not containing mutations there), and their appropriate mutations.
-        relevant_mutations = pd.merge(b, m, on='read', how='left').fillna(0)
-            
-        # only keep reads with WT or known mutations. Discard reads with unknown mutations in the checked positions.
-        # reads to drop:
-        reads_to_drop = m[~(m.full_mutation.isin(recognized_mutations))].read.tolist()
-        # dropping:
-        relevant_mutations = relevant_mutations[~(relevant_mutations.read.isin(reads_to_drop))]
+        print(mutation_1)
         matrix.at['P(column)', mutation_1] = len(relevant_mutations[relevant_mutations.full_mutation == mutation_1][['read']].drop_duplicates()) / len(relevant_mutations.read.drop_duplicates())
         matrix.at['P(column)', mutation_1[1:-1].split('.')[0]] = 1 - (len(relevant_mutations[relevant_mutations.full_mutation == mutation_1][['read']].drop_duplicates()) / len(relevant_mutations.read.drop_duplicates()))
         for mutation_2 in recognized_mutations:
             print (mutation_1, mutation_2)
-            b = blast_df.copy()
-            m = mutations_df.copy()
-            # keep only alignments containing both positions for mutations 1 and 2.
-            b = b[(b.start_ref < int(mutation_1[1:-1].split('.')[0])) & (b.end_ref > int(mutation_1[1:-1].split('.')[0])) & (b.start_ref < int(mutation_2[1:-1].split('.')[0])) & (b.end_ref > int(mutation_2[1:-1].split('.')[0]))]
-            b = b[['read']]
-            # look only at mutations in positions i and j
-            m = m[m.position.isin([int(mutation_1[1:-1].split('.')[0]), int(mutation_2[1:-1].split('.')[0])])]
-            # keep all reads that contain positions i and j (both containing mutations in i and/or j 
-            # and not containing mutations there), and their appropriate mutations.
-            relevant_mutations = pd.merge(b, m, on='read', how='left').fillna(0)
-            
-            # only keep reads with WT or known mutations. Discard reads with unknown mutations in the checked positions.
-            # reads to drop:
-            reads_to_drop = m[~(m.full_mutation.isin(recognized_mutations))].read.tolist()
-            # dropping:
-            relevant_mutations = relevant_mutations[~(relevant_mutations.read.isin(reads_to_drop))]
-            
             # calculate conditional probabilities
             reads_with_mut_1 = len(relevant_mutations[relevant_mutations.full_mutation == mutation_1][['read']].drop_duplicates())
             reads_with_wt_1 = len(relevant_mutations[['read']].drop_duplicates()) - reads_with_mut_1
@@ -86,8 +63,7 @@ def mutation_pairings_statistics(args):
             matrix.at[mutation_1, mutation_2[1:-1].split('.')[0]] = reads_with_mut_1_and_wt_2 / reads_with_mut_1
             matrix.at[mutation_1[1:-1].split('.')[0], mutation_2[1:-1].split('.')[0]] = reads_with_wt_1_and_wt_2 / reads_with_wt_1
             matrix.at[mutation_1[1:-1].split('.')[0], mutation_2] = reads_with_wt_1_and_mut_2 / reads_with_wt_1
-    matrix.to_csv(args.output_file)
-            
+    matrix.to_csv(args.output_file)       
     return
 
 
