bfc068e2d13e7af2d5a033d3941a258070c03fba
Noam Harel <noamharel@power9login.tau.ac.il>
2020-07-05 11:33:53 +0300

    added dealing with .Ds dirs

--- association_tests/unify_association_results.py
+++ association_tests/unify_association_results.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         parser.print_help()
         parser.exit(1)
     dfs = []
-    files = [args.input_results_directory + '/' + f + '/chi2_results.csv' for f in os.listdir(args.input_results_directory)]
+    files = [args.input_results_directory + '/' + f + '/chi2_results.csv' for f in os.listdir(args.input_results_directory) if not f.startswith('.')]
     for f in tqdm(files):
         dfs.append(pd.read_csv(f))
     df = pd.concat(dfs)
